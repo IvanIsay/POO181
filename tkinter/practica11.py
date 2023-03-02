73224909f8615bc817ebce811cf2d7d1062dd321
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-03-01 18:03:26 -0600

    Funciones para botones

--- tkinter/practica11.py
+++ tkinter/practica11.py
@@ -1,5 +1,21 @@
 
-from tkinter import Tk,Frame,Button
+from tkinter import Tk,Frame,Button,messagebox
+
+#4.Funcion de mensajes para el boton
+def mostraMensaje():
+    messagebox.showinfo("Aviso","Este mensaje es para informar algo")
+    messagebox.showerror("Error:","Todo fallo con exito")
+    print(messagebox.ask("Pregunta:","El o ella jugo con tu corazon"))
+
+
+#5. Funcion para agregar Botones
+def agregarBoton():
+    botonVerde.config(text="+",bg="green",fg="white")
+    botonNuevo= Button(seccion3,text="Boton Nuevo")
+    botonNuevo.pack()
+    
+
+
 
 #1.Instaciamos un Objeto Ventana
 ventana= Tk()
@@ -16,7 +32,7 @@ seccion2.pack(expand=True,fill='both')
 seccion3=Frame(ventana,bg="#ff99cc")
 seccion3.pack(expand=True,fill='both')
 #3.botones
-botonAzul= Button(seccion1,text="boton azul",fg="blue")
+botonAzul= Button(seccion1,text="boton azul",fg="blue",command=mostraMensaje )
 botonAzul.place(x=60, y=60)
 
 botonAmarillo= Button(seccion2,text="boton amarillo",bg="#ffff1a")
@@ -25,7 +41,7 @@ botonAmarillo.grid(row=0, column=0)
 botonNegro= Button(seccion2,text="boton negro",fg="white",bg="black")
 botonNegro.grid(row=1, column=1)
 
-botonVerde= Button(seccion3,text="boton Verde",bg="#239023")
+botonVerde= Button(seccion3,text="boton Verde",bg="#239023",command= agregarBoton )
 botonVerde.pack()
 
 
